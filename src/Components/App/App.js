9b028e115388f655cbda3c40bd32183af4aaf7a7
Dustin-Har <harbaughdustin@yahoo.com>
2021-08-02 16:05:30 -0400

    Add fetch call in app

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,27 @@
 import React, { useState, useEffect } from 'react'
+import getStories from '../../ApiCall/Apicall'
 
 export default function App() {
-  const [ stories, setStories ] = useState()
+  const [ stories, setStories ] = useState([])
+  const [ error, setError ] = useState('')
+
+
+  // arts, automobiles, books, business, fashion, food, health, home, insider, magazine, movies, nyregion, obituaries, opinion, politics, realestate, science, sports, sundayreview, technology, theater, t-magazine, travel, upshot, us, and world
+  useEffect(() => {
+    const fetchStories = async () => {
+      try {
+        const response = await getStories('automobiles');
+        setStories(response.results)
+      } catch(e) {
+        setError(e.message)
+      }
+    }
+    fetchStories()
+  }, [])
+
   return (
-    <div>
+    <main>
       
-    </div>
+    </main>
   )
 }
