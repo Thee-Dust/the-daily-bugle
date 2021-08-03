70af45df7f800c4cd9b1b7f636a914881eb48440
Dustin-Har <harbaughdustin@yahoo.com>
2021-08-03 13:04:57 -0400

    Add navbar select tag

--- src/ApiCall/Apicall.js
+++ src/ApiCall/Apicall.js
@@ -1,5 +1,5 @@
+const key = process.env.REACT_APP_NY_API_KEY;
 export default async function getStories(selection) {
-  const key = process.env.REACT_APP_NY_API_KEY
   try{
     const response = await fetch(`https://api.nytimes.com/svc/topstories/v2/${selection}.json?api-key=${key}`);
     const data = errorHandler(response);
--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,17 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import getStories from '../../ApiCall/Apicall'
+import Navbar from '../Navbar/Navbar'
+// import { Switch, Route } from 'react-router-dom'
 
 export default function App() {
   const [ stories, setStories ] = useState([])
   const [ error, setError ] = useState('')
 
 
-  // arts, automobiles, books, business, fashion, food, health, home, insider, magazine, movies, nyregion, obituaries, opinion, politics, realestate, science, sports, sundayreview, technology, theater, t-magazine, travel, upshot, us, and world
   useEffect(() => {
     const fetchStories = async () => {
       try {
-        const response = await getStories('automobiles');
-        setStories(response.results)
+        const topStories = await getStories('automobiles');
+        setStories(topStories.results)
       } catch(e) {
         setError(e.message)
       }
@@ -21,7 +22,7 @@ export default function App() {
 
   return (
     <main>
-      
+      <Navbar/>
     </main>
   )
 }
